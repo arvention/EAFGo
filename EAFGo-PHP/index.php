80fe02a2dae8bdcfd35ca9b66f881a5e15fba89c
arvention <amc.antioquia@yahoo.com.ph>
2015-11-16 22:14:22 +0800

    Login Initial

--- EAFGo-PHP/index.php
+++ EAFGo-PHP/index.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>EAF Go | De La Salle University</title>
+        <link rel="shortcut icon" href="img/dlsu_logo.png" />
+        <link rel="stylesheet" type="text/css" 	media="all" href="css/Login.css" />
+        <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+        <!--script type="text/javascript" src ="js/login.js"></script-->
     </head>
     <body>
         <?php
-        echo "Hello World";
+        echo "Hello";
         ?>
     </body>
 </html>
