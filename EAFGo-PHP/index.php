d3cfc65e67ee1645bf99f2c7e9fea86393fcb8ef
arvention <amc.antioquia@yahoo.com.ph>
2015-11-16 23:39:51 +0800

    Login basic CSS

--- EAFGo-PHP/index.php
+++ EAFGo-PHP/index.php
@@ -4,13 +4,24 @@
         <meta charset="UTF-8">
         <title>EAF Go | De La Salle University</title>
         <link rel="shortcut icon" href="img/dlsu_logo.png" />
-        <link rel="stylesheet" type="text/css" 	media="all" href="css/Login.css" />
+        <link rel="stylesheet" type="text/css" 	media="all" href="css/login.css" />
         <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
         <!--script type="text/javascript" src ="js/login.js"></script-->
     </head>
     <body>
-        <?php
-        echo "Hello";
-        ?>
+        <div id = "overallDiv">
+            <div id ="loginDiv">
+                <div id="loginLogo" class = "loginBoxCenter">
+                    <img src = "img/dlsu_logo.png" height="75px" width="75px"/>
+                </div>
+                
+                <h1 id = "eafLabel" class = "loginBoxCenter">eaf<div id="goLabel">go</div></h1>
+                <form id = "loginForm" class = "loginBoxCenter" action = "Signup" method = "post">
+                    <input type="text" name ="id" placeholder = "ID" class = "loginFields" required/>
+                    <input type="password" name = "password" placeholder = "Password" id = "passwordField" class = "loginFields" required/>
+                    <input type ="submit" id = "loginButton" class = "clickableButton" value="Let's start!"/>
+                </form>
+            </div>
+        </div>
     </body>
 </html>
